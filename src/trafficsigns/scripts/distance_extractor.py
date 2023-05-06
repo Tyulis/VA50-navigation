Fix effect of lag in traffic signs

src/trafficsigns/scripts/distance_extractor.py
@@ -16,6 +16,7 @@
 
 import sys
 import cProfile
+from threading import Lock
 
 import yaml
 import cv2 as cv
@@ -31,7 +32,7 @@ from sensor_msgs.msg import Image, PointCloud2, CameraInfo
 
 import fish2bird
 from trafficsigns.msg import TrafficSign, TrafficSignStatus
-from visualization.msg import VizUpdate
+from transformtrack.srv import TransformBatch, TransformBatchRequest
 
 from traffic_sign_detection import TrafficSignDetector
 from traffic_light_detection import detect_traffic_lights
@@ -77,8 +78,14 @@ class DistanceExtractor (object):
 		self.image_stamp = None
 		self.pointcloud_stamp = None
 
+		rospy.loginfo("Waiting for the TransformBatch service...")
+		self.transform_service = None
+		rospy.wait_for_service(self.parameters["node"]["transform-service-name"])
+		self.transform_service = rospy.ServiceProxy(self.parameters["node"]["transform-service-name"], TransformBatch, persistent=True)
+		self.transform_service_lock = Lock()
+
 		# Initialize the topic subscribers
-		self.image_subscriber = rospy.Subscriber(self.image_topic, Image, self.callback_image)
+		self.image_subscriber = rospy.Subscriber(self.image_topic, Image, self.callback_image, queue_size=1, buff_size=2**28)
 		self.camerainfo_subscriber = rospy.Subscriber(self.camerainfo_topic, CameraInfo, self.callback_camerainfo)
 		self.pointcloud_subscriber = rospy.Subscriber(self.pointcloud_topic, PointCloud2, self.callback_pointcloud)
 		self.visualization_publisher = rospy.Publisher(self.visualization_topic, Image, queue_size=10)
@@ -105,6 +112,51 @@ class DistanceExtractor (object):
 			np.concatenate((rotation_matrix, translation_vector), axis=1),
 			np.asarray((0, 0, 0, 1)).reshape((1, 4))
 		), axis=0)
+
+	def get_map_transforms(self, start_times, end_time):
+		"""Get a batch of transforms of the vehicle frame from the given `start_times` to `end_time`
+		   - start_times : list<rospy.Time> : Timestamps to get the transforms from
+		   - end_time    : rospy.Time       : Target timestamp, to get the transforms to
+		<----------------- ndarray[N, 4, 4] : 3D homogeneous transform matrices to transform points in the vehicle frame
+		                                      at `start_times[i]` to the vehicle frame at `end_time`
+		<----------------- list<rospy.Time> : Unbiased start times. This is an artifact from the time when the simulator gave incoherent timestamps,
+		                                      same as `start_times` on the latest versions
+		<----------------- rospy.Time       : Unbiased end time, same as end_time on the latest versions of the simulator
+		"""
+		# Build the request to the transform service, see srv/TransformBatch.srv and msg/TimeBatch.msg for info
+		request = TransformBatchRequest()
+		request.start_times = start_times
+		request.end_time = end_time
+
+		# Now send the request and get the response
+		# We use persistent connections to improve efficiency, and ROS advises to implement some reconnection logic
+		# in case the network gets in the way, so in case of disconnection, retry 10 times to reconnect then fail
+		tries = 0
+		while True:
+			try:
+				# Apparently, when a call to the service is pending, the node is free to service other callbacks,
+				# including callback_trafficsign that also call this service
+				# So with the traffic signs subscriber active, it’s only a matter of time until both get to their transform service call concurrently
+				# For some reason, ROS allows it, and for some reason it deadlocks ROS as a whole
+				# So let’s throw in a lock to prevent ROS from killing itself
+				with self.transform_service_lock:
+					response = self.transform_service(request)
+				break
+			except rospy.ServiceException as exc:
+				if tries > 10:
+					rospy.logerr(f"Connection to service {self.parameters['node']['transform-service-name']} failed {tries} times, skipping")
+					rospy.logerr(f"Failed with error : {exc}")
+					raise RuntimeError("Unable to connect to the transform service")
+				rospy.logerr(f"Connection to service {self.parameters['node']['transform-service-name']} lost, reconnecting...")
+				self.transform_service.close()
+				self.transform_service = rospy.ServiceProxy(self.parameters["node"]["transform-service-name"], TransformBatch, persistent=True)
+				tries += 1
+		
+		# The call was successful, get the transforms in the right format and return
+		# The transpose is because the individual matrices are transmitted in column-major order
+		transforms = np.asarray(response.transforms.data).reshape(response.transforms.layout.dim[0].size, response.transforms.layout.dim[1].size, response.transforms.layout.dim[2].size).transpose(0, 2, 1)
+		distances = np.asarray(response.distances)
+		return transforms, distances
 
 
 	def callback_image(self, data):
@@ -164,15 +216,15 @@ class DistanceExtractor (object):
 		img = self.latest_image
 		img_stamp = self.image_stamp
 
+		transforms, distances = self.get_map_transforms([pointcloud_stamp], img_stamp)
+		pointcloud = transforms[0] @ pointcloud
+
 		self.pointcloud_array = []
 		self.pointcloud_stamp_array = []
 
 		lidar_coordinates_in_image = self.lidar_to_image(pointcloud)
 
 		camera_pointcloud = self.lidar_to_camera @ pointcloud
-
-		# Calculate the distance to each lidar point
-		distances = np.linalg.norm(pointcloud, axis=0)
 
 		# Get the annotated image and detected traffic signs labels and coordinates
 		img, traffic_signs = self.traffic_sign_detector.get_traffic_signs(img)